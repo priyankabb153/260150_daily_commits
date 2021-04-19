025a9267cee1fdd5b6db9225266263a9c9cddc6e
Priyanka Bhadrappanavar <v.bhadrappanavar@f5.com>
2021-04-19 16:13:48 +0530

    updated boolean program from pycharm in windows environment

---
+++ boolean.py
@@ -0,0 +1,24 @@
+def print_boolean():
+    # Use a breakpoint in the code line below to debug your script.
+    my_boolean = True
+    print(my_boolean)
+
+    print(2 == 3)
+
+    print("hello" == "hello")
+
+# comparison or relational operators
+    print(1 != 1)
+
+    print("eleven" != "seven")
+
+    print(2 != 10)
+
+    print(7 >= 5)
+
+    print(9 >= 9.0)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_boolean()
